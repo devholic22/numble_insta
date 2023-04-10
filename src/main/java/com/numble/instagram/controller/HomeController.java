6f312607a54538812bfad24f92dac2f1acc261de
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-04-11 00:39:03 +0900

    Update HomeController.java

--- src/main/java/com/numble/instagram/controller/HomeController.java
+++ src/main/java/com/numble/instagram/controller/HomeController.java
@@ -14,4 +14,9 @@ public class HomeController {
 
         return ResponseEntity.ok("Hello");
     }
+
+    @GetMapping("/actuator/health")
+    public String health() {
+        return "health";
+    }
 }
